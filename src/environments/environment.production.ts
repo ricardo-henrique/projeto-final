852062f8c0eedbe210c7303f17478a2ab4c5fe1b
ricardo-henrique <ricardohenrique2013.1@gmail.com>
2025-07-22 23:09:40 -0300

    add my api link to prod enviroment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://cms-hardware-api.netlify.app/api/v1',
+  apiUrl: 'https://projeto-final-api-vyy0.onrender.com/api/v1',
   debugMode: false,
 };
